919bafa50e3a6ad035ade7bdb66a46665ee5ef73
Sajid Nawaz Khan <ssnkhan@users.noreply.github.com>
2021-11-18 12:30:01 +0000

    Update shutdown_vectr.sh

--- shutdown_vectr.sh
+++ shutdown_vectr.sh
@@ -1,7 +1,7 @@
-# Save this in your home folder, to quickly shutdown vectr
+# Save this in your home folder, to quickly shutdown VECTR
 # Sajid Nawaz Khan, 06/09/2020
 # Adversarial Threat Modelling — A Practical Approach to Purple Teaming in the Enterprise
-# https://github.com/ssnkhan/x33fcon
+# https://github.com/ssnkhan/adversarial-threat-modelling
 
 cd /opt/vectr
 sudo docker-compose down
