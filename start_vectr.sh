e72657a21a1797d22293c024bd29841f770dec5d
Sajid Nawaz Khan <ssnkhan@users.noreply.github.com>
2024-09-25 20:45:15 +0100

    Delete `start_vectr.sh`
    No longer required following the 2024 refresh.

--- start_vectr.sh
+++
@@ -1,9 +0,0 @@
-# Save this in your home folder, to quickly launch VECTR
-# Sajid Nawaz Khan, 06/09/2020
-# Adversarial Threat Modelling — A Practical Approach to Purple Teaming in the Enterprise
-# https://github.com/ssnkhan/adversarial-threat-modelling
-
-cd /opt/vectr
-sudo docker-compose up -d
-sleep 30
-firefox "https://sravectr.internal:8081/sra-purpletools-webui/app/#" &
